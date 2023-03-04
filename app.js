fb6ad3ce9760ddeb05d487bfcf1d3475b9c08812
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-03-04 09:30:15 +0530

    changes in main

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+//read
 const fs = require('fs');
 fs.readFile('file.txt', 'utf8', function (err, data) {
  if (err) throw err;
  console.log(data);
+});
+
+//write
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
 });

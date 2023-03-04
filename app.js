727f309811040df9ee1ff001602a956d891eefd4
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-03-04 09:55:34 +0530

    changes

--- app.js
+++ app.js
@@ -37,3 +37,30 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 //module
 const myModule = require('./my-module.js');
 console.log(myModule.myFunction());
+
+//promises
+
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+    resolve('Success!');
+    } else {
+    reject('Failure!');
+    }
+   });
+   myPromise.then((result) => {
+    console.log(result);
+   }).catch((error) => {
+    console.log(error);
+   });
+
+   //async/await
+
+   async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+   }
+   myFunction();
